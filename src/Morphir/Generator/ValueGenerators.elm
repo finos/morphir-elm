41fba40cd5fbec33fe3a1b99ada3ed8c9a83a686
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-07-24 13:40:10 -0500

    Fix errors in Elm around Generator (#1170)

--- src/Morphir/Generator/ValueGenerators.elm
+++ src/Morphir/Generator/ValueGenerators.elm
@@ -1,9 +1,9 @@
-module Morphir.Elm.Generator.ValueGenerators exposing (boolValueGenerator, charValueGenerator, floatValueGenerator, fromType, intValueGenerator, listValueGenerator, maybeValueGenerator, recordValueGenerator, stringValueGenerator, tupleValueGenerator)
+module Morphir.Generator.ValueGenerators exposing (boolValueGenerator, charValueGenerator, floatValueGenerator, fromType, intValueGenerator, listValueGenerator, maybeValueGenerator, recordValueGenerator, stringValueGenerator, tupleValueGenerator)
 
 import Date
 import Dict exposing (Dict)
-import Morphir.Elm.Generator.API as Generator exposing (Generator)
-import Morphir.IR as IR exposing (IR)
+import Morphir.Generator.API as Generator exposing (Generator)
+import Morphir.IR.Distribution exposing (Distribution, lookupTypeSpecification)
 import Morphir.IR.FQName as FQName
 import Morphir.IR.Literal as Literal
 import Morphir.IR.Name exposing (Name)
@@ -55,7 +55,7 @@ fromType ir tpe =
         Type.Reference _ (( typePackageName, typeModuleName, _ ) as fQName) typeArgs ->
             -- Handle references that are not part of the SDK
             ir
-                |> IR.lookupTypeSpecification fQName
+                |> lookupTypeSpecification fQName
                 |> Result.fromMaybe (String.concat [ "Cannot find reference: ", FQName.toString fQName ])
                 |> Result.andThen
                     (\typeSpec ->
