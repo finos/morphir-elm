eadfe4e3c362c6f0f28258a3cf0497d63f4358fd
Nwokafor Choongsaeng <nwokafor.choongsaeng@morganstanley.com>
2025-02-14 08:08:59 +0000

    fix broken ref

--- morphir-ts/src/exports/ir.ts
+++ morphir-ts/src/exports/ir.ts
@@ -2,6 +2,7 @@ import { Morphir_IR_AccessControlled } from "../generated/morphir/ir/AccessContr
 import { Morphir_IR_Distribution } from "../generated/morphir/ir/Distribution";
 import { Morphir_IR_Documented } from "../generated/morphir/ir/Documented";
 import { Morphir_IR_FQName } from "../generated/morphir/ir/FQName";
+import { Morphir_IR_FormatVersion } from "../generated/morphir/ir/FormatVersion";
 import { Morphir_IR_Literal } from "../generated/morphir/ir/Literal";
 import { Morphir_IR_Module } from "../generated/morphir/ir/Module";
 import { Morphir_IR_Name } from "../generated/morphir/ir/Name";
@@ -10,7 +11,6 @@ import { Morphir_IR_Path } from "../generated/morphir/ir/Path";
 import { Morphir_IR_QName } from "../generated/morphir/ir/QName";
 import { Morphir_IR_Type } from "../generated/morphir/ir/Type";
 import { Morphir_IR_Value } from "../generated/morphir/ir/Value";
-import { Morphir_IR_VersionedDistribution } from "../generated/morphir/ir/VersionedDistribution";
 
 export import AccessControlled = Morphir_IR_AccessControlled;
 
@@ -19,6 +19,8 @@ export import Distribution = Morphir_IR_Distribution;
 export import Documented = Morphir_IR_Documented;
 
 export import FQName = Morphir_IR_FQName;
+
+export import FormatVersion = Morphir_IR_FormatVersion;
 
 export import Literal = Morphir_IR_Literal;
 
@@ -35,5 +37,3 @@ export import QName = Morphir_IR_QName;
 export import Type = Morphir_IR_Type;
 
 export import Value = Morphir_IR_Value;
-
-export import VersionedDistribution = Morphir_IR_VersionedDistribution;
