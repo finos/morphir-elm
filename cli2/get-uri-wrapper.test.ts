f5daeeec1b699ec33ae5018fb1fc135744d470d4
AttilaMihaly <Attila.Mihaly@MorganStanley.com>
2025-02-18 16:50:14 +0100

    Fix test

--- cli2/get-uri-wrapper.test.ts
+++ cli2/get-uri-wrapper.test.ts
@@ -9,7 +9,7 @@ describe('the get-uri-wrapper module', () => {
     test("that fetches a document and converts it to JSON", async () => {
         let file = path.resolve("./elm.json")
         let elmJson = await getUriWrapper.fetchUriToJson(`file://${file}`);
-        expect(elmJson).toHaveProperty("compilerOptions");
+        expect(elmJson).toHaveProperty("type");
  
     });
 });
